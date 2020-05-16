5c07fda54fb1e5ae2e64098a6ac24542dfaedcd8
Mridula Singh <mrdlsngh@gmail.com>
2020-05-16 18:52:23 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ShoppingcartComponent } from './components/shoppingcart/shoppingcart.component';
+import { FiltersComponent } from './components/shoppingcart/filters/filters.component';
+import { ProductListComponent } from './components/shoppingcart/product-list/product-list.component';
+import { CartComponent } from './components/shoppingcart/cart/cart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ShoppingcartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
